e9384e39aa9c30ea91e38a62c677696e74c5de85
thejus557 <suryatejakandukuru8@gmail.com>
2024-08-25 18:43:47 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "NexGen.AI",
   description: "Your Intelligent Companion for Smarter Conversations.",
-  author: "Surya teja",
+  authors: "Surya teja",
   role: "Senior Frontend Developer"
 };
 
