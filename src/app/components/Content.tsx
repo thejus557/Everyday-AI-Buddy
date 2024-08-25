bb5531e9dcada40cb9e7ebdd5f1f8b391335e893
Surya <suryateja.kandukuru@techolution.com>
2024-08-25 10:49:25 +0530

    fix: authentication middleware

--- src/app/components/Content.tsx
+++ src/app/components/Content.tsx
@@ -14,21 +14,21 @@ const Content = () => {
   };
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 justify-evenly items-center w-full">
+    <div className="container grid grid-cols-1 md:grid-cols-2 justify-evenly items-center w-full">
       <div className="grid md:order-none order-2 justify-center items-center h-fit">
-        <div className="grid text-center md:text-center items-center text-4xl md:text-6xl lg:text-8xl font-bold text-highlight my-2">
+        <div className="grid text-center md:text-left items-center text-4xl md:text-6xl lg:text-8xl font-bold text-highlight my-2">
           Jarvis AI
         </div>
-        <div className="grid text-center md:text-center items-center text-base md:text-xl lg:text-2xl my-2">
+        <div className="grid text-center md:text-left items-center text-base md:text-xl lg:text-2xl my-2">
           Your Intelligent Partner for Daily Tasks, Insights, and Creativity.
         </div>
         <Button
-          className="!m-auto"
+          className="m-auto md:m-0"
           title="Get Started"
           onClick={handleGetStarted}
         />
       </div>
-      <div className="grid md:order-none order-1 justify-center">
+      <div className="grid md:order-none order-1 justify-evenly">
         <Lottie
           loop
           animationData={lottieJson}
--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -19,7 +19,7 @@ const Navbar = () => {
       className={`flex w-full items-center justify-center bg-white h-[64px]`}
     >
       <div className="container">
-        <div className="relative -mx-4 flex items-center justify-between">
+        <div className="relative mx-4 flex items-center justify-between">
           <div className="w-60 max-w-full px-4">
             <a href="/" className="block w-full py-5">
               <div className="text-highlight">Jarvis AI</div>
@@ -39,17 +39,17 @@ const Navbar = () => {
                 <span className="relative my-[6px] block h-[2px] w-[30px] bg-body-color "></span>
               </button>
             </div>
-            <div className="hidden justify-end pr-16 sm:flex lg:pr-0">
+            <div className="justify-end flex lg:pr-0">
               <SignedOut>
                 <a
                   href="/sign-in"
-                  className="px-7 py-3 text-base font-medium text-dark hover:text-primary "
+                  className="p-2 text-base font-medium text-dark hover:text-primary "
                 >
                   Sign in
                 </a>
                 <a
                   href="/sign-up"
-                  className="rounded-md bg-primary px-7 py-3 text-base font-medium text-white hover:bg-primary/90"
+                  className="rounded-md bg-primary p-2 text-base font-medium text-white hover:bg-primary/90"
                 >
                   Sign Up
                 </a>
